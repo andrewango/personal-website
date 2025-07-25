5747d115a7abddfdf97f90e12b657231c3cb3555
unknown <ango@udel.edu>
2025-07-24 21:17:36 -0400

    Add new ocean background and underwater scroll effect

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,16 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import bg from '../images/mist.gif';
+import bg from '../images/ocean.gif';
+import bg2 from '../images/underwater.gif';
 
 export default function Background() {
+  const [isPastHero, setIsPastHero] = useState(false);
+
+  useEffect(() => {
+    const heroSection = document.getElementById('hero');
+
+    const handleScroll = () => {
+      if (!heroSection) return;
+      const rect = heroSection.getBoundingClientRect();
+      setIsPastHero(rect.top + (rect.height / 2) < 0);;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div
-      className="fixed top-0 left-0 w-full h-full -z-10 opacity-50 blur-md overflow-hidden"
-    >
-      <img
-        src={bg}
-        alt="bg"
-        className="w-full h-full object-cover"
-      />
-    </div>
+    <>
+      <div
+        className={`fixed top-0 left-0 w-full h-full -z-20 transition-opacity duration-500 ease-in-out ${
+          isPastHero ? 'opacity-0' : 'opacity-50'
+        } blur-md overflow-hidden`}
+      >
+        <img
+          src={bg}
+          alt="background"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div
+        className={`fixed top-0 left-0 w-full h-full -z-30 transition-opacity duration-500 ease-in-out ${
+          isPastHero ? 'opacity-50' : 'opacity-0'
+        } blur-md overflow-hidden`}
+      >
+        <img
+          src={bg2}
+          alt="underwater cave"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </>
   );
 }
